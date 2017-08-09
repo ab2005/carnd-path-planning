858a12acde7ae4cc693127aca315b76d51dd9291
AB <ab2005@gmail.com>
2017-08-09 12:53:09 -0700

    make car move in it's lane

--- src/main.cpp
+++ src/main.cpp
@@ -230,8 +230,8 @@ int main() {
           double dist_inc = 0.5;
           for (int i = 0; i < 50; i++) {
             double next_s = car_s + (i + 1) * dist_inc;
-            next_x_vals.push_back(car_x + (dist_inc * i) * cos(deg2rad(car_yaw)));
-            next_y_vals.push_back(car_y + (dist_inc * i) * sin(deg2rad(car_yaw)));
+            double next_d = 6; // stay in line
+            vector<double> xy = getXY(next_s, next_d, map_waypoints_x, map_waypoints_y);
           }
           
           json msgJson;
